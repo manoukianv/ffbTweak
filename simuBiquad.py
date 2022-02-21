7ec8d04a698844a61f7acfeb65d5315edc0c67b9
Vincent Manoukian <10980775+manoukianv@users.noreply.github.com>
2022-02-21 17:48:13 +0100

    Add the combobox to select samples.

--- simuBiquad.py
+++ simuBiquad.py
@@ -142,24 +142,34 @@ class DataGen(object):
         self.x1 = 0
         self.x2 = 0
 
-    def reset_series(self):
-        #samples1 = [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 5
-        #samples2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 2
-        #self.samples = samples1 + samples2
-        #self.timing = np.arange(0.0, len(self.samples), 1)
-        
-        #Sin
-        #timing1 = np.arange(0.0, 150, 1)
-        #samples1 = np.sin(timing1 / 10)
-        #samples2 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 6)
-        #timing2 = np.arange(len(samples1), len(samples1)+len(samples2), 1)
-        #self.samples = np.concatenate((samples1, samples2))
-        #self.timing = np.concatenate((timing1, timing2))
-
-        self.samples = np.random.randint(1,100,500)
-        self.timing = np.arange(0.0, len(self.samples), 1)
-        self.filtered_samples = list(map(self._compute_sample,self.samples))
-
+    def reset_series(self, selection):
+        if selection == 0:
+            #Min value
+            samples1 = [0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 5
+            samples2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 2
+            self.samples = samples1 + samples2
+            self.timing = np.arange(0.0, len(self.samples), 1)
+            self.filtered_samples = list(map(self._compute_sample,self.samples))
+        elif selection == 1:
+            #Max value
+            samples1 = [32768] * 140
+            samples2 = [0] * 140
+            self.samples = samples1 + samples2
+            self.timing = np.arange(0.0, len(self.samples), 1)
+            self.filtered_samples = list(map(self._compute_sample,self.samples))
+        elif selection == 2:
+            #Sin
+            timing1 = np.arange(0.0, 150, 1)
+            samples1 = np.sin(timing1 / 10)
+            samples2 = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] * 6)
+            timing2 = np.arange(len(samples1), len(samples1)+len(samples2), 1)
+            self.samples = np.concatenate((samples1, samples2))
+            self.timing = np.concatenate((timing1, timing2))
+            self.filtered_samples = list(map(self._compute_sample,self.samples))
+        elif selection == 3:
+            self.samples = np.random.randint(1,100,500)
+            self.timing = np.arange(0.0, len(self.samples), 1)
+            self.filtered_samples = list(map(self._compute_sample,self.samples))
 
     def setUpCoefficient(self, a0, a1, a2, b1, b2):
         self.a0 = a0
@@ -178,6 +188,9 @@ class DataGen(object):
         self.z2 = i * self.a2 - self.b2 * out
         return out
 
+    def _compute_avg(self,i):
+        self.avg_sum += i
+
 
 class ControlBox(wx.Panel):
     """ A static box with a couple of radio buttons and a text
@@ -187,7 +200,7 @@ class ControlBox(wx.Panel):
     def __init__(self, parent, ID, label):
         wx.Panel.__init__(self, parent, ID)
         self.type = 0
-        self.sample = 2000
+        self.sample = 1000
         self.fc = 20
         self.Q = 1
         self.gain = -30
@@ -202,11 +215,6 @@ class ControlBox(wx.Panel):
             size=(70,-1),
             choices = type,
             style=wx.TE_PROCESS_ENTER)        
-
-        #self.manual_text0 = wx.TextCtrl(self, -1,
-        #    size=(70,-1),
-        #    value=str(self.type),
-        #    style=wx.TE_PROCESS_ENTER)
             
         self.manual_text1 = wx.TextCtrl(self, -1,
             size=(70,-1),
@@ -228,19 +236,12 @@ class ControlBox(wx.Panel):
             value=str(self.gain),
             style=wx.TE_PROCESS_ENTER)
 
-        #self.Bind(wx.EVT_UPDATE_UI, self.on_update_manual_text, self.manual_text)
         self.Bind(wx.EVT_COMBOBOX, self.on_text_enter_type, self.typeeffect)
         self.Bind(wx.EVT_TEXT, self.on_text_enter_sample, self.manual_text1)
         self.Bind(wx.EVT_TEXT, self.on_text_enter_fc, self.manual_text2)
         self.Bind(wx.EVT_TEXT, self.on_text_enter_Q, self.manual_text3)
         self.Bind(wx.EVT_TEXT, self.on_text_enter_gain, self.manual_text4)
-        
 
-        #manual_box = wx.BoxSizer(wx.HORIZONTAL)
-        #manual_box.Add(self.radio_manual, flag=wx.ALIGN_CENTER_VERTICAL)
-        #manual_box.Add(self.manual_text, flag=wx.ALIGN_CENTER_VERTICAL)
-
-        #sizer.Add(self.radio_auto, 0, wx.ALL, 10)
         sizer.Add(wx.StaticText(self,-1,"Type"), 0, wx.ALIGN_CENTER, 5)
         sizer.Add(self.typeeffect, 0, wx.ALIGN_CENTER, 5)
         sizer.Add(wx.StaticText(self,-1,"Freq. sample"), 0, wx.ALIGN_CENTER, 5)
@@ -319,43 +320,33 @@ class GraphFrame(wx.Frame):
         self.init_plot()
         self.canvas = FigCanvas(self.panel, -1, self.fig)
 
-        self.xmin_control = ControlBox(self.panel, -1, "A1 / A2 / B1 / B2")
+
+        self.xmin_control = ControlBox(self.panel, -1, "Biquad Setting")
+        
+        samples_name = ['min', 'max', 'sin', 'random'] 
+        self.typesample = wx.ComboBox(self.panel, -1,
+            size=(70,-1),
+            choices = samples_name,
+            style=wx.TE_PROCESS_ENTER)   
+
 
         self.compute_button = wx.Button(self.panel, -1, "Compute")
         self.Bind(wx.EVT_BUTTON, self.on_compute_button, self.compute_button)
-        self.Bind(wx.EVT_UPDATE_UI, self.on_update_compute_button, self.compute_button)
 
-        #self.cb_grid = wx.CheckBox(self.panel, -1,
-        #    "Show Grid",
-        #    style=wx.ALIGN_RIGHT)
-        #self.Bind(wx.EVT_CHECKBOX, self.on_cb_grid, self.cb_grid)
-        #self.cb_grid.SetValue(True)
-
-        #self.cb_xlab = wx.CheckBox(self.panel, -1,
-        #    "Show X labels",
-        #    style=wx.ALIGN_RIGHT)
-        #self.Bind(wx.EVT_CHECKBOX, self.on_cb_xlab, self.cb_xlab)
-        #self.cb_xlab.SetValue(True)
         self.axes.grid(True, color='gray')
 
         self.hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox1.Add( self.typesample, border =10, flag=wx.ALL)
         self.hbox1.Add(self.compute_button, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
-        #self.hbox1.AddSpacer(20)
-        #self.hbox1.Add(self.cb_grid, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
-        #self.hbox1.AddSpacer(10)
-        #self.hbox1.Add(self.cb_xlab, border=5, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL)
 
         self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        self.hbox2.Add(self.xmin_control, border=5, flag=wx.ALL)
-        #self.hbox2.Add(self.xmax_control, border=5, flag=wx.ALL)
-        #self.hbox2.AddSpacer(24)
-        #self.hbox2.Add(self.ymin_control, border=5, flag=wx.ALL)
-        #self.hbox2.Add(self.ymax_control, border=5, flag=wx.ALL)
+        self.hbox2.Add(self.xmin_control, border=10, flag=wx.ALL)
 
         self.vbox = wx.BoxSizer(wx.VERTICAL)
         self.vbox.Add(self.canvas, 1, flag=wx.LEFT | wx.TOP | wx.GROW)
         self.vbox.Add(self.hbox2, 0, flag=wx.ALIGN_LEFT | wx.TOP)
         self.vbox.Add(self.hbox1, 0, flag=wx.ALIGN_LEFT | wx.TOP)
+
 
         self.panel.SetSizer(self.vbox)
         self.vbox.Fit(self)
@@ -383,60 +374,6 @@ class GraphFrame(wx.Frame):
     def draw_plot(self):
         """ Redraws the plot
         """
-        # when xmin is on auto, it "follows" xmax to produce a
-        # sliding window effect. therefore, xmin is assigned after
-        # xmax.
-        #
-        #if self.xmax_control.is_auto():
-        #    xmax = len(self.data) if len(self.data) > 50 else 50
-        #else:
-        #    xmax = int(self.xmax_control.manual_value())
-
-        #if self.xmin_control.is_auto():
-        #    xmin = xmax - 50
-        #else:
-
-
-        # for ymin and ymax, find the minimal and maximal values
-        # in the data set and add a mininal margin.
-        #
-        # note that it's easy to change this scheme to the
-        # minimal/maximal value in the current display, and not
-        # the whole data set.
-        #
-        #if self.ymin_control.is_auto():
-        #    ymin = round(min(self.data), 0) - 1
-        #else:
-        #    ymin = int(self.ymin_control.manual_value())
-
-        #if self.ymax_control.is_auto():
-        #    ymax = round(max(self.data), 0) + 1
-        #else:
-        #    ymax = int(self.ymax_control.manual_value())
-
-        #self.axes.set_xbound(lower=xmin, upper=xmax)
-        #self.axes.set_ybound(lower=ymin, upper=ymax)
-
-        # anecdote: axes.grid assumes b=True if any other flag is
-        # given even if b is set to False.
-        # so just passing the flag into the first statement won't
-        # work.
-        #
-        #if self.cb_grid.IsChecked():
-        #    self.axes.grid(True, color='gray')
-        #else:
-        #    self.axes.grid(False)
-
-        # Using setp here is convenient, because get_xticklabels
-        # returns a list over which one needs to explicitly
-        # iterate, and setp already handles this.
-        #
-        #pylab.setp(self.axes.get_xticklabels(),
-        #    visible=self.cb_xlab.IsChecked())
-
-        #self.plot_data.set_xdata(np.arange(len(self.data)))
-        #self.plot_data.set_ydata(np.array(self.data))
-
         xmin = round(min(self.datagen.timing), 0) - 1
         xmax = round(max(self.datagen.timing), 0) + 1
         ymin = round(min(self.datagen.samples), 0) - 0.2
@@ -461,14 +398,11 @@ class GraphFrame(wx.Frame):
         self.filter.setBiquad(type, fc / sample, Q, gain)
 
         self.datagen.setUpCoefficient(self.filter.a0, self.filter.a1, self.filter.a2, self.filter.b1, self.filter.b2)
-        self.datagen.reset_series()
+        selection = self.typesample.GetSelection()
+        self.datagen.reset_series(selection)
         self.draw_plot()
         print("a0:" + str(self.filter.a0) + " a1:" + str(self.filter.a1) + " a2:" + str(self.filter.a2) + 
             " b1:" + str(self.filter.b1) + " b2:" + str(self.filter.b2))
-
-    def on_update_compute_button(self, event):
-        #update the input label here if required
-        i=1
 
     def on_cb_grid(self, event):
         self.draw_plot()
